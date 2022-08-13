7b2da7fb905807c9a180d94671b0590f29d6ee32
nipunbandara <nipun.bandara@outlook.com>
2022-08-14 01:42:40 +0530

    Add completed RemoteServer

---
+++ RemoteServer/DataServer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using DataBase;
+
+namespace RemoteServer
+{
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, UseSynchronizationContext = false)]
+    internal class DataServer : DataServerInterface
+    {
+        public DataServer()
+        {
+            //DatabaseClass users = new DatabaseClass();
+        }
+        public int GetNumEntries()
+        {
+            DatabaseClass users = new DatabaseClass();
+            return users.GetNumRecords();
+        }
+        public void GetValuesForEntry(int index, out uint acctNo, out uint pin, out int bal, out string fName, out string lName)
+        {
+            DatabaseClass users = new DatabaseClass();
+            acctNo = users.GetAcctNoByIndex(index);
+            pin = users.GetPINByIndex(index);
+            bal = users.GetBalanceByIndex(index);
+            fName = users.GetFirstNameByIndex(index);
+            lName = users.GetLastNameByIndex(index);
+        }
+        
+    }
+}
---
+++ RemoteServer/DataServerInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteServer
+{
+    [ServiceContract]
+    public interface DataServerInterface
+    {
+        [OperationContract]
+        int GetNumEntries();
+        [OperationContract]
+        void GetValuesForEntry(int index, out uint acctNo, out uint pin, out int bal, out string fName, out string lName);
+    }
+}
